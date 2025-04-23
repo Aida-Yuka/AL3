ac11f40465edf8858e282b3897481be74501f08e
Aida_Yuka <K024G0052@g.neec.ac.jp>
2025-04-23 15:22:26 +0900

    01-03

--- GameScene.cpp
+++ GameScene.cpp
@@ -1,18 +1,71 @@
 #include "GameScene.h"
+// #include "ImGuiManager.h"
 
 using namespace KamataEngine;
 
-void GameScene::Initialize()
-{
+void GameScene::Initialize() {
 	///インゲームの初期化処理///
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+
+	//サウンドデータの読み込み
+	soundDateHandle_ = Audio::GetInstance()->LoadWave("fanfare.wav");
+
+	//3Dモデル
+	model_ = Model::Create();
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	//カメラの初期化
+	camera_.Initialize();
+
+	//音声再生
+	//Audio::GetInstance()->PlayWave(soundDateHandle_);
+
+	voiceHandle_ = Audio::GetInstance()->PlayWave(soundDateHandle_, true);
 }
 
-void GameScene::Update()
-{
+void GameScene::Update() {
 	///インゲームの更新処理///
+
+	//スペースキーを押した瞬間
+	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+		// 音声停止
+		Audio::GetInstance()->StopWave(voiceHandle_);
+	}
+
+	/*/デバッグテキストの表示
+	ImGui::Begin("Debug1");
+
+	//float3入力ボックス
+	//ImGui::InputFloat3("InputFloat3", inputFloat3);
+
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+
+	ImGui::End();*/
 }
 
-void GameScene::Draw()
-{
+void GameScene::Draw() {
 	///描画処理///
+
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	//3Dモデル描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	///モデルインスタンスの描画処理///
+
+	//3Dモデル描画
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+
+	///＝＝＝＝＝＝＝＝＝＝＝＝＝＝///
+
+	//3Dモデル描画後処理
+	Model::PostDraw();
 }
+
+//デストラクタ
+GameScene::~GameScene() { delete model_; }
--- main.cpp
+++ main.cpp
@@ -1,41 +1,50 @@
-#include <Windows.h>
-#include "KamataEngine.h"
 #include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
 
 using namespace KamataEngine;
 
-// Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
-{
+//Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//エンジンの初期化
 	KamataEngine::Initialize(L"LE2D_01_アイダ_ユウカ_AL3");
-	
-	//DirectXCommonインスタンスの取得
-	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-	
+
 	//ゲームシーンのインスタンス生成
 	GameScene* gameScene = new GameScene();
 
 	//ゲームシーンの初期化
 	gameScene->Initialize();
 
-	// メインループ
-	while (true)
-	{
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	//InguiManagerインスタンスの取得3
+	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
+
+	//メインループ
+	while (true) {
 		//エンジンの更新
-		if (KamataEngine::Update())
-		{
+		if (KamataEngine::Update()) {
 			break;
 		}
 
+		//ImGui受付開始
+		imguiManager->Begin();
+
 		//ゲームシーンの更新
 		gameScene->Update();
+
+		//ImGui受付終了
+		imguiManager->End();
 
 		//描画開始
 		dxCommon->PreDraw();
 
 		//ゲームシーンの描画
 		gameScene->Draw();
+
+		//ImGui描画
+		imguiManager->Draw();
 
 		//描画終了
 		dxCommon->PostDraw();
