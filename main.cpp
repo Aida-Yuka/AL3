b52a85a2cf780555cb663444364d8d77cd5d5df8
Aida_Yuka <K024G0052@g.neec.ac.jp>
2025-05-07 18:50:06 +0900

    02-02

--- main.cpp
+++ main.cpp
@@ -19,12 +19,17 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 	//ゲームシーンの初期化
 	gameScene->Initialize();
 
-	// メインループ
-	while (true)
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	//InguiManagerインスタンスの取得3
+	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
+
+	//メインループ
+	while (true) 
 	{
 		//エンジンの更新
-		if (KamataEngine::Update())
-		{
+		if (KamataEngine::Update()) {
 			break;
 		}
 
